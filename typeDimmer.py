882b771e7cfe97216da86622a2289876c3dc8f9b
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-04-17 00:20:58 +0200

    two classes for two types of devices

---
+++ typeDimmer.py
@@ -0,0 +1,45 @@
+import json
+
+class typeDimmer:
+    keyWords = {
+        "ein" : 1,
+        "an" : 1,
+        "aus" : 0,
+        "ab" : 0,
+        "auf" : 2,
+        "zu" : 2
+    }
+
+    def __init__(self,name,client):
+        self.name = name
+        self.client = client
+
+    def parseCMD(self,cmd):
+        keyWord = list()
+        numWord = int()
+        for key in cmd:
+            if key in self.keyWords:
+                keyWord.append(self.keyWords[key])
+            elif key.isdigit():
+                numWord = int(key)
+        if numWord and (2 in keyWord):
+            self.__setDim(numWord)
+        elif 0 in keyWord:
+            self.__turnOff()
+        elif 1 in keyWord:
+            self.__turnOn()
+
+    def __turnOn(self):
+        cmd = dict(cmd = dict(brightness = 100))
+        self.__send(cmd)
+
+    def __turnOff(self):
+        cmd = dict(cmd = dict(brightness = 0))
+        self.__send(cmd)
+
+    def __setDim(self,lum):
+        cmd = dict(cmd = dict(brightness = lum))
+        self.__send(cmd)
+
+    def __send(self,cmd):
+        self.client.publish("/actu/" + self.name + "/cmd",json.dumps(cmd))
---
+++ typeRGB.py
@@ -0,0 +1,58 @@
+import json
+from getColors import colorEng, colorGer, keyWords
+
+class typeRGB:
+
+
+    def __init__(self,name,client):
+        self.name = name
+        self.client = client
+
+    def parseCMD(self,cmd):
+        keyWord = list()
+        colorWord = str()
+        numWord = int()
+        for key in cmd:
+            if(key in keyWords):
+                keyWord.append(keyWords[key])
+            elif(key in colorGer):
+                colorWord = colorGer[key]
+            elif(key in colorEng):
+                colorWord = colorEng[key]
+            elif key.isdigit():
+                numWord = int(key)
+        if colorWord and numWord and (2 in keyWord):
+            self.__setCaD(colorWord, numWord)
+        elif colorWord:
+            self.__setColor(colorWord)
+        elif numWord and (2 in keyWord):
+            self.__setDim(numWord)
+        elif (0 in keyWord):
+            self.__turnOff()
+        elif (1 in keyWord):
+            self.__turnOn()
+
+
+    def __turnOn(self):
+        cmd = dict(cmd = dict(brightness = 100))
+        self.__send(cmd)
+
+    def __turnOff(self):
+        cmd = dict(cmd = dict(brightness = 0))
+        self.__send(cmd)
+
+    def __setDim(self,lum):
+        cmd = dict(cmd = dict(brightness = lum))
+        self.__send(cmd)
+
+    def __setColor(self, color):
+        cmd = dict(cmd = dict(color = int(color[1:],16)))
+        self.__send(cmd)
+
+    def __setCaD(self, color, lum):
+        cmd = dict(cmd = dict(color = int(color[1:],16)))
+        cmd["cmd"]["brightness"] = lum
+        self.__send(cmd)
+
+    def __send(self,data):
+        self.client.publish("/actu/" + self.name + "/cmd",json.dumps(data))
